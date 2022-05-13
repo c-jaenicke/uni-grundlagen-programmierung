06ae38618ea7d5d0e2877364f17a5abf3f072d0f
c-jaenicke <c-jaenicke@pm.me>
2022-05-13 10:29:35 +0200

    livecoding

---
+++ LiveCoding-10-05-2022/main.java
@@ -0,0 +1,20 @@
+/**
+ * main
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println(add(12,5));
+
+        idk(6,20);
+    }
+
+    private static void idk(int x, int y){
+    
+    System.out.println( (x>=y) ? y : x ) ;
+    }    
+        
+    private static int add(int a,int b){
+        return a + b;
+    }
+}
