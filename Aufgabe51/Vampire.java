46b350481d3f6eb53be9ed9a1aa6b335890e0de6
c-jaenicke <c-jaenicke@pm.me>
2022-06-07 11:51:56 +0200

    anfang

---
+++ Aufgabe51/Vampire.java
@@ -0,0 +1,17 @@
+/**
+ * Vampir
+ */
+public class Vampire {
+
+    String firstName;
+    String lastName;
+    int yearOfBirth;
+
+    // constructor
+    public Vampire(String firstName, String lastName, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+}
---
+++ Aufgabe51/VampireTest.java
@@ -0,0 +1,11 @@
+/**
+ * VampireTest
+ */
+public class VampireTest {
+
+    public static void main(String[] args) {
+    
+        Vampir eins = new Vampire("Klaus", "Peter", 2000)        
+
+    }
+}
